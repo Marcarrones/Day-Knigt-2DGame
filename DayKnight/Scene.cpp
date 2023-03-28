3854a1e91e018297c8aa8038007bc31f878f485a
Marc <marcduchb20@gmail.com>
2023-03-28 20:30:18 +0200

    Added check for completed map

--- DayKnight/Scene.cpp
+++ DayKnight/Scene.cpp
@@ -6,8 +6,8 @@
 #include "Text.h"
 
 
-#define SCREEN_X 32
-#define SCREEN_Y 16
+// #define SCREEN_X 32
+// #define SCREEN_Y 16
 
 #define INIT_PLAYER_X_TILES 4
 #define INIT_PLAYER_Y_TILES 25
@@ -67,7 +67,15 @@ void Scene::update(int deltaTime)
 	}
 
 	if (player->collider.CheckColission(enemy->collider)) printf("COLISION!");
+
+	// Check if map cleared
+	if (map->remainingTiles() <= 0) {
+		changelevel(2);
+	}
+
 }
+
+// TODO: Remove duped function
 
 void Scene::changescreen(int screen) {
 	map = TileMap::createTileMap("levels/level01.txt", glm::vec2(SCREEN_X, SCREEN_Y), texProgram);
