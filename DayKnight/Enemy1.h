69facb3a552ab91d0e4d8176e0ecdfc9e32c9adb
Marc <marcduchb20@gmail.com>
2023-04-04 20:59:24 +0200

    Enemy1 Logic (skeleton)

--- DayKnight/Enemy1.h
+++ DayKnight/Enemy1.h
@@ -7,18 +7,22 @@ class Enemy1 : public Entity
 {
 public:
 	Enemy1();
-	~Enemy1();
 
 	void init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram);
 	
 	virtual void update(int deltaTime);
+	void changeDirection();
 	void render();
 
 	void setTileMap(TileMap *tileMap);
-	void setPosition(const glm::vec2 &pos);
+	//void setPosition(const glm::vec2 &pos);
+
+	int enemySpeed = 3;
 
 private:
 	void initSprite(ShaderProgram &shaderProgram);
+
+	int movementDir = 1; // 1 -> Right : -1 -> Left
 };
 
 #endif /// _ENEMY1_
