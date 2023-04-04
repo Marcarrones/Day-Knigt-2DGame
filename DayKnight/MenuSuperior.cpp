207c68487d0213fe7f9a58fa940e7d8be4a781c4
Marc <marcduchb20@gmail.com>
2023-04-04 21:01:41 +0200

    Implemented scale (changed text size from 2->1)

--- DayKnight/MenuSuperior.cpp
+++ DayKnight/MenuSuperior.cpp
@@ -12,22 +12,19 @@ MenuSuperior::~MenuSuperior() {
 
 void MenuSuperior::init(glm::ivec2 pos, ShaderProgram& shaderProgram)
 {
-	shader = shaderProgram;
-	int high = 0;
+	//shader = shaderProgram;
+	//int high = 0;
 	spriteVit = new Sprite();
 	spritesheetVit.loadFromFile("images/vida.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	spriteVit = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(1, 1), &spritesheetVit, &shaderProgram);
 	spriteVit->setPosition(glm::vec2(float(pos.x -16 ), float(pos.y -38 )));
 	text = new Text[6];
-	text[0].init(glm::vec2(float(pos.x + 16), float(pos.y - 32)), shaderProgram, 2,  "X");
-	text[1].init(glm::vec2(float(pos.x + 36), float(pos.y - 32)), shaderProgram, 2, "03");
-	text[2].init(glm::vec2(float(pos.x + 90), float(pos.y - 32)), shaderProgram, 2, "000000");
-	text[3].init(glm::vec2(float(pos.x + 230), float(pos.y - 32)), shaderProgram, 2, "000000");
-	text[4].init(glm::vec2(float(pos.x + 400), float(pos.y - 32)), shaderProgram, 2, "STAGE");
-	text[5].init(glm::vec2(float(pos.x + 520), float(pos.y - 32)), shaderProgram, 2, "01");
-	
-
-
+	text[0].init(glm::vec2(float(pos.x + 16), float(pos.y - 32)), shaderProgram, 1,  "X");
+	text[1].init(glm::vec2(float(pos.x + 36), float(pos.y - 32)), shaderProgram, 1, "03");
+	text[2].init(glm::vec2(float(pos.x + 90), float(pos.y - 32)), shaderProgram, 1, "000000");
+	text[3].init(glm::vec2(float(pos.x + 230), float(pos.y - 32)), shaderProgram, 1, "000000");
+	text[4].init(glm::vec2(float(pos.x + 400), float(pos.y - 32)), shaderProgram, 1, "STAGE");
+	text[5].init(glm::vec2(float(pos.x + 520), float(pos.y - 32)), shaderProgram, 1, "01");
 }
 
 void MenuSuperior::restart() {
